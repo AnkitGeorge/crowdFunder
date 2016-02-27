f317281192feee601804b9f67c52ac620895dbc7
AnkitGeorge <ankit.george@gmail.com>
2016-02-27 16:25:30 -0500

    Fix foreign key, required params, and only registered users being able to create a project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+
+  before_filter :require_login, except: [:index, :show]
+  # you need to be logged in, except when viewing all projects, or a specific project show page.
+
   def index
     @projects = Project.all
   end
@@ -9,6 +13,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.owner_id = current_user.id
 
     if @project.save
       redirect_to projects_url
@@ -19,12 +24,12 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @comment = Comment.new(user: current_user, project: @project)
+    #@comment = Comment.new(user: current_user, project: @project)
   end
 
   private
   def project_params
-    params.require(:project).permit(:title, :description, :funding_goal, :project_end_date)
+    params.require(:project).permit(:title, :description, :owner_id, :funding_goal, :project_end_date)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :comments
   has_many :pledges
-  has_many :pledged_projects, through: :pledges, :source => "Project"
+  has_many :pledged_projects, through: :pledges, :source => :project
   has_many :owned_projects, :class_name => "Project", :foreign_key => :owner_id
 
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes["password"] }
